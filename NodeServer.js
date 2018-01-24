ceda5bbf8efe74e6a2dc25a8176fabd3980a1d04
Jay Dickson <jaydickson02@gmail.com>
2018-01-24 23:12:08 +1100

    Completed linking of programs

--- NodeServer.js
+++ NodeServer.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var nodemailer = require('nodemailer');
+var expy = require('./ExecutePython');
 
 http.createServer(function(req, res) {
   fs.readFile('index.html', function(err, data) {
@@ -11,31 +12,52 @@ http.createServer(function(req, res) {
     res.write(data);
     res.end();
     var qdata = url.parse(req.url, true).query;
+
     if (qdata['key'] == 'true') {
 
+      var myPythonScriptPath = 'RandomSubreddit.py';
 
-      var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'dev.jaydickson02@gmail.com',
-    pass: ''
-  }
-});
+      // Use python shell
+      var PythonShell = require('python-shell');
+      var pyshell = new PythonShell(myPythonScriptPath);
 
-var mailOptions = {
-  from: 'dev.jaydickson02@gmail.com',
-  to: 'wizzie405@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+      pyshell.on('message', function(message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'dev.jaydickson02@gmail.com',
+            pass: '02jd.pass0706'
+          }
+        });
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+        var mailOptions = {
+          from: 'dev.jaydickson02@gmail.com',
+          to: 'wizzie405@gmail.com',
+          subject: 'Your Random SubReddit!',
+          text: message
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+      });
+
+      // end the input stream and allow the process to exit
+      pyshell.end(function(err) {
+        if (err) {
+          throw err;
+        };
+
+        console.log('finished');
+      });
+
+
+
 
     };
   })
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Requires the running of "npm install nodemailer --save" in the command line to install node dependancys first.
 
 Also requires the python module Praw. Run pip install praw in the command line to accomplish this.
+
+Other requirements:
+
+npm install python-shell
