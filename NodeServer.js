e22c068ba22ee87476eb828a4c71a0c2013506a9
Jay Dickson <jaydickson02@gmail.com>
2018-01-30 21:10:56 +1100

    Simplified

--- NodeServer.js
+++ NodeServer.js
@@ -3,46 +3,21 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var SendMail = require('./SendEmail');
-//Http server for dealing with connections
+var SendHtml = require('./SendHtml');
+var queryPython = require('./queryPython');
+
+//Code Starts Here
 http.createServer(function(req, res) {
-  //Read html
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
-    //Send html to client
-    res.write(data);
-    res.end();
-    //Parse the url to check for requests
-    var qdata = url.parse(req.url, true).query;
-    //Confirms a request was placed
-    if (qdata['key'] == 'true') {
-      //Executes python script and reads response
-      var myPythonScriptPath = 'RandomSubreddit.py';
 
-      // Use python shell
-      var PythonShell = require('python-shell');
-      var pyshell = new PythonShell(myPythonScriptPath);
-      //received a message sent from the Python script (a simple "print" statement)
-      pyshell.on('message', function(message) {
+  global.res = res;
 
-        SendMail.email(message, 'wizzie405@gmail.com');
-
-      });
-
-      // end the input stream and allow the process to exit
-      pyshell.end(function(err) {
-        if (err) {
-          throw err;
-        };
-
-        console.log('finished');
-      });
+  fs.readFile('index.html', SendHtml.Send);
+  var qdata = url.parse(req.url, true).query;
 
 
+  if (qdata['key'] == 'true') {
 
+    queryPython.query('RandomSubreddit.py')
+  };
 
-    };
-  })
-  //Listens on port 80 for incoming connections from external ips
 }).listen(80, '0.0.0.0');
